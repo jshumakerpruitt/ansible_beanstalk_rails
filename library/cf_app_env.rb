73e9d4c00442691b626891d77e2956585df10d9e
Jasper <jshumakerpruitt@gmail.com>
2016-09-12 17:17:17 +0700

    add beanstalk cf role

--- library/cf_app_env.rb
+++ library/cf_app_env.rb
@@ -2,9 +2,39 @@ CloudFormation do
   Description("Template to create EB App+Env")
   AWSTemplateFormatVersion("2010-09-09")
 
+  Parameter("SolutionStackName") do
+    Description("Solution Stack Name")
+    Type("String")
+  end
+
+  Parameter('KeyName') do
+    Description("EC2 Key Name for SSH Access")
+    Type('AWS::EC2::KeyPair::KeyName')
+  end
+
+  Parameter('EnvironmentName') do
+    Description("Environment name e.g. rails-staging")
+    Type('String')
+  end
+
+  Parameter('ApplicationName') do
+    Description("Application name e.g. my-app")
+    Type('String')
+  end
+
+  Parameter('ASGMin') do
+    Description("Min number of instances in ASG")
+    Type('Number')
+  end
+
+  Parameter('ASGMax') do
+    Description("Max number of instances in ASG")
+    Type('Number')
+  end
+
   Resource("BeanstalkApplication") do
     Type("AWS::ElasticBeanstalk::Application")
-    Property("ApplicationName", "BeanstalkApplication") 
+    Property("ApplicationName", Ref("ApplicationName"))
     Property("Description", "EB application")
   end
 
@@ -16,27 +46,46 @@ CloudFormation do
                {
                  "Namespace"  => "aws:autoscaling:asg",
                  "OptionName" => "MinSize",
-                 "Value"      => "2"
+                 "Value"      => Ref("ASGMin")
                },
                {
                  "Namespace"  => "aws:autoscaling:asg",
                  "OptionName" => "MaxSize",
-                 "Value"      => "6"
+                 "Value"      => Ref("ASGMax")
+               },
+               {
+                 "Namespace"  => "aws:autoscaling:launchconfiguration",
+                 "OptionName" => "EC2KeyName",
+                 "Value"      => Ref("KeyName")
                },
                {
                  "Namespace"  => "aws:elasticbeanstalk:environment",
                  "OptionName" => "EnvironmentType",
                  "Value"      => "LoadBalanced"
+               },
+               {
+                 "Namespace"  => "aws:elb:loadbalancer",
+                 "OptionName" => "CrossZone",
+                 "Value"      => "true"
+               },
+               {
+                 "Namespace"  => "aws:elasticbeanstalk:environment",
+                 "OptionName" => "LoadBalancerType",
+                 "Value"      => "application"
                }
              ])
-    Property("SolutionStackName", "64bit Amazon Linux running PHP 5.3")
+    Property("SolutionStackName", Ref("SolutionStackName"))
   end
 
   Resource("BeanstalkEnvironment") do
     Type("AWS::ElasticBeanstalk::Environment")
     Property("ApplicationName", Ref("BeanstalkApplication"))
     Property("Description", "AWS Elastic Beanstalk Environment")
+    Property("EnvironmentName", Ref("EnvironmentName") )
     Property("TemplateName", "DefaultConfiguration")
-    Property("VersionLabel", "Initial Version")
+    Property("Tier", {
+               "Name" => "WebServer",
+               "Type" => "Standard"
+               })
   end
 end
